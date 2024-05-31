6651ff9ef46c90b54a6bb4dd5ca0827572c6a125
Matthias De Lozzo <matthias.delozzo@IRT-AESE.local>
2024-05-31 11:21:24 +0200

    add custom ml algo

---
+++ src/lh2pac/gemseo/custom_ml_algo.py
@@ -0,0 +1,22 @@
+from gemseo.core.discipline import MDODiscipline
+from gemseo.utils.data_conversion import concatenate_dict_of_arrays_to_array
+from gemseo.utils.data_conversion import split_array_to_dict_of_arrays
+
+
+class MyMLAlgo(MDODiscipline):
+    def __init__(self, algo, input_names, output_names):
+        self.algo = algo
+        super().__init__()
+        self.input_grammar.update_from_names(input_names)
+        self.output_grammar.update_from_names(output_names)
+
+    def _run(self) -> None:
+        input_data = self.get_input_data()
+        input_vector = concatenate_dict_of_arrays_to_array(
+            input_data, self.input_grammar.names
+        )
+        output_vector = self.algo.predict(input_vector)
+        output_data = split_array_to_dict_of_arrays(
+            output_vector, dict.fromkeys(self.output_grammar.names, 1)
+        )
+        self.store_local_data(**output_data)
